b35d246b0840edc0f77bd8c029d2edaae2f82daa
Christopher-Drifter <chris@cjinternationalservices.com>
2019-09-15 16:02:55 +0200

    Use process.cwd() to get root directory

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-/* Magic Import
+/* Imports
  *
  * Automagically import files with simpler syntax, ignoring subdirectories in
- * nodejs.
- *
- * There are two caveats:
- *
- * 1. All filenames must be unique
- * 2. require("magic-import") must be called in the root directory before
- *    anywhere else
+ * nodejs. ALL FILENAMES MUST BE UNIQUE!
  *
  * Type:
  *
- *    const { FileName, FileNameSubDir } = require("magic-import");
+ *    const { FileName, FileNameSubDir } = require("./imports");
  *
  * instead of:
  *
@@ -24,46 +18,72 @@
 const fs = require("fs");
 const path = require("path");
 
+/* Helpers
+ */
+// returns true if dir contains /node_modules/
+const isNodeModules = dir => {
+   return `${dir}/`.match(/[\/\\]node_modules[\/\\]/);
+};
+// returns absolute filepath from dir & file
+const getFilepath = (dir, file) => {
+   return path.join(dir, file);
+};
+// returns true if filepath references a directory
+const isDirectory = filepath => {
+   return fs.statSync(filepath).isDirectory();
+};
+// returns true if filepath references a javascript/json file
+const isJavascriptFile = filepath => {
+   return [".js", ".json"].includes(path.extname(filepath));
+};
+// returns the file key (the filename without an extension)
+const getFileKey = filepath => {
+   return path.basename(filepath).replace(path.extname(filepath), "");
+};
+
 /* Setup/Config
  */
-const filesByKey = {};
-const dir = __dirname;
+const filepathsByKey = {};
+const dir = process.cwd();
 const extensions = [".js", ".json"];
 
 /* Read all the .js/.json files in this directory and below it
  */
 const files = (function* walker(dir) {
+   // ignore node modules
+   if (isNodeModules(dir)) return;
+
    const files = fs.readdirSync(dir);
 
    for (const file of files) {
-      const pathToFile = path.join(dir, file);
-      const isDirectory = fs.statSync(pathToFile).isDirectory();
-      if (isDirectory) {
-         yield* walker(pathToFile);
+      const filepath = getFilepath(dir, file);
+
+      if (isDirectory(filepath)) {
+         yield* walker(filepath);
          continue;
       }
 
-      yield pathToFile;
+      if (isJavascriptFile(filepath)) {
+         yield filepath;
+         continue;
+      }
    }
 })(dir);
 
-/* Add all paths to object in form key => pathToFile
+/* Add all paths to object in form key => filepath
  * eg. Filename => FileName.js
  */
-for (let pathToFile of files) {
-   const filename = path.basename(pathToFile);
-   const extension = path.extname(pathToFile);
-   const key = filename.replace(extension, "");
-
-   filesByKey[key] = pathToFile;
+for (let filepath of files) {
+   const key = getFileKey(filepath);
+   filepathsByKey[key] = filepath;
 }
 
 /* Use a proxy to require the filepath and return the module
- * when a property is requested on filesByKey object
+ * when a property is requested on filepathsByKey object
  */
-const proxy = new Proxy(filesByKey, {
-   get: (filesByKey, key) => {
-      const path = filesByKey[key];
+const proxy = new Proxy(filepathsByKey, {
+   get: (filepathsByKey, key) => {
+      const path = filepathsByKey[key];
       return path ? require(path) : undefined;
    }
 });
